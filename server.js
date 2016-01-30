f74db2ac52a8194974fbcecff6a9566b7a23d508
Kevin Chen <kevin521@gmail.com>
2016-01-29 19:27:53 -0800

    Adding instagram api

--- server.js
+++ server.js
@@ -5,7 +5,43 @@ var express = require('express'),
 
 var app = express();
 
+var ig = require('instagram-node').instagram();
+ 
+// Every call to `ig.use()` overrides the `client_id/client_secret` 
+// or `access_token` previously entered if they exist. 
+// ig.use({ access_token: 'YOUR_ACCESS_TOKEN' }); need this for user auth, who gives a shit
+ig.use({ client_id: 'f5f94de0bf0b48569685b79d4e615332',
+         client_secret: 'c1e75801a1ad41f3b568a8f195121abe' });
+
+
+var redirect_uri = 'http://expediahackathon.azurewebsites.net/handleauth';
+ 
+exports.authorize_user = function(req, res) {
+  res.redirect(ig.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+ 
+exports.handleauth = function(req, res) {
+  ig.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+    }
+  });
+};
+
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/handleauth', exports.handleauth);
+
+
 app.get('/', function(req, res){
+	ig.tag_search('boobs', function(err, result, remaining, limit) {
+			console.log(result);
+	});
   res.render('index')
 });
 
